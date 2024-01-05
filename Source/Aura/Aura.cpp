21f6151dda19511d49ad99f9fababd687c6b3dea
zhuzi <zhuziyang123456@gmail.com>
2024-01-05 16:10:17 +0800

    add character base class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// zhuz1's learning project
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// zhuz1's learning project
+
+#pragma once
+
+#include "CoreMinimal.h"
+
